07ee84cad2695610f128e87f24e8cd6cc27d87f2
bfuster <brunofuster@gmail.com>
2010-12-30 14:38:48 -0200

    readme and startup files

--- Classes/Resource.m
+++ Classes/Resource.m
@@ -1,10 +1,24 @@
-//
-//  Resource.m
-//  restfulie-objc
-//
-//  Created by Bruno Fuster on 10/22/10.
-//  Copyright 2010 Caelum. All rights reserved.
-//
+/*
+ * Resource.m
+ * restfulie-objc
+ *
+ * Created by Bruno Fuster on 10/22/10.
+ * 
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <objc/runtime.h>
 #import "NSObject+Properties.h"
@@ -32,27 +46,23 @@
 	
 	/* CHECK FOR ID KEYS */
 	
-	for (NSString *key in [mutableDict allKeys]) {
-		if (![obj respondsToSelector:NSSelectorFromString(key)]) {
-			NSLog(@"didnt respond to %@", key);
+	for (NSString *key in [mutableDict allKeys]) 
+		if (![obj respondsToSelector:NSSelectorFromString(key)]) 
 			[mutableDict removeObjectForKey:key];
-		} else {
-			NSLog(@"responded to %@", key);
-		}
-	}
 	
-	/* GO AGAIN FOR A REAL CHANGE */
+	/* PREPARE DICT */
 	
 	for (NSString *key in [mutableDict allKeys]) {
+		
 		id i = [mutableDict objectForKey:key];
+		
 		if ([i isKindOfClass:[NSDictionary class]]) {
 			
-			const char *className = [obj typeOfPropertyNamed:key];
-			NSString *classNameString = [NSString stringWithUTF8String:className];
-				
-			NSString *sanitize = [classNameString substringWithRange:NSMakeRange(3, ([classNameString length] -4))];
-			NSLog(@"%i ", [classNameString length]);
-			Class compositionClass = NSClassFromString(sanitize);
+			NSString *classNameString = [NSString stringWithUTF8String:[obj typeOfPropertyNamed:key]];
+			NSString *className = [classNameString substringWithRange:NSMakeRange(3, ([classNameString length] -4))];
+			
+			Class compositionClass = NSClassFromString(className);
+			
 			if (compositionClass != nil) {
 				id obj = [self createObjWithDictionary:i ofType:compositionClass];
 				[mutableDict setObject:obj forKey:key];
@@ -70,10 +80,7 @@
 	
 	for (id obj in array) 
 		[objsArray addObject:[self createObjWithDictionary:obj ofType:clazz]];
-	
-	//NSArray *result = [[NSArray alloc] initWithArray:objsArray];
-	//[objsArray release];
-	
+		
 	return objsArray;
 }
 
--- Classes/Response.m
+++ Classes/Response.m
@@ -1,13 +1,26 @@
-//
-//  Response.m
-//  restfulie-objc
-//
-//  Created by Bruno Fuster on 10/22/10.
-//  Copyright 2010 Caelum. All rights reserved.
-//
+/*
+ * Response.m
+ * restfulie-objc
+ *
+ * Created by Bruno Fuster on 10/22/10.
+ * 
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "Response.h"
-#import "RestObj.h"
 
 @implementation Response
 
@@ -25,13 +38,13 @@
 {
 	
 	Resource *resource = [[Resource alloc] initWithData:self.data ofType:clazz];
-	//TODO create a real wrapper for Resource!
+	//TODO create a real wrapper for a Resource!
 	return resource;
 }
 
 - (id) deserialize
 {
-	NSLog(@"something %@", self.data);
+	NSLog(@"data %@", self.data);
 	return nil;
 }
 
